4227616a1ba6655afda5c5e718dc7aa1930a289d
Luke Morales <lukemorales@live.com>
2023-06-29 22:09:39 -0300

    Declare brand symbol consistently

--- src/shared/validation.ts
+++ src/shared/validation.ts
@@ -3,11 +3,11 @@ import { z } from 'zod';
 export const Email = z.string().email().brand('Email');
 export type Email = z.infer<typeof Email>;
 
-export const Password = z.string().min(6).brand<'Password'>();
+export const Password = z.string().min(6).brand('Password');
 export type Password = z.infer<typeof Password>;
 
 export const EncryptedPassword = z
   .string()
   .length(60)
-  .brand<'EncryptedPassword'>();
+  .brand('EncryptedPassword');
 export type EncryptedPassword = z.infer<typeof EncryptedPassword>;
